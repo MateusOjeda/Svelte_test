bab3a3d16ebd9991d438b90672781f378f01c685
Mateus Ojeda <mateus.ojeda.alv@gmail.com>
2022-06-10 00:40:57 -0300

    Initial commit - adding nav bar and routes

---
+++ src/lib/nav.svelte
@@ -0,0 +1,73 @@
+<script>
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+
+  let hovering = false;
+
+  onMount(async () => {});
+
+  let menuItems = [
+    { route: "/", name: "Home" },
+    { route: "/1on1", name: "1:1" },
+    { route: "/feedback", name: "Feedback" },
+    { route: "/map", name: "Mapa" },
+    { route: "/pdi", name: "PDI" },
+  ];
+</script>
+
+<div
+  class="nav"
+  on:mouseenter={() => (hovering = true)}
+  on:mouseleave={() => (hovering = false)}
+>
+  <div class="vertical-menu {hovering ? '' : 'small'}">
+    {#each menuItems as { route, name }}
+      <a href={route} class={$page.url.pathname === route ? "active" : ""}>
+        <img src="/controller.svg" alt="site logo" />
+        {#if hovering}
+          {name}
+        {/if}
+      </a>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .nav {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: var(--ytd-mini-guide-width);
+    height: 100%;
+    background-color: rgb(78, 0, 109);
+  }
+  .vertical-menu {
+    width: 200px;
+  }
+
+  .small {
+    width: 50px;
+  }
+
+  .vertical-menu a {
+    background-color: #eee;
+    color: black;
+    display: flex;
+    align-items: center;
+    padding: 12px;
+    text-decoration: none;
+  }
+
+  .vertical-menu a:hover {
+    background-color: #ccc;
+  }
+
+  .vertical-menu a.active {
+    background-color: #04aa6d;
+    color: white;
+  }
+
+  .vertical-menu img {
+    margin-right: 10px;
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Nav from "../lib/nav.svelte";
+  import "../styles/global.css";
+</script>
+
+<header />
+<main>
+  <Nav />
+  <slot />
+</main>
+<footer>
+  <p>Copyright 2022 Mateus Ojeda</p>
+</footer>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+  }
+  main {
+    max-width: 960px;
+    margin: 20px auto;
+  }
+  footer {
+    text-align: center;
+  }
+</style>
